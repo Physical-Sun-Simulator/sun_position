d0bdd9fcc4fde44a1e253f8e41abdd2c20214e5d
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2024-05-27 12:22:46 +0200

    Attempted to fix the pip config

---
+++ sun_position/__init__.py
@@ -0,0 +1,10 @@
+from sun_position.sun_position import get_sun_elevation_angle as get_sun_elevation_angle
+from sun_position.sun_position import get_sun_azimuth_angle as get_sun_azimuth_angle
+from sun_position.sun_position import get_sun_declination as get_sun_declination
+from sun_position.sun_position import get_hour_angle as get_hour_angle
+from sun_position.sun_position import get_true_solar_time as get_true_solar_time
+from sun_position.sun_position import get_equation_of_time as get_equation_of_time
+from sun_position.sun_position import get_rad_day as get_rad_day
+from sun_position.sun_position import get_time_sunset as get_time_sunset
+from sun_position.sun_position import get_time_sunrise as get_time_sunrise
+from sun_position.sun_position import get_daylight_duration as get_daylight_duration
